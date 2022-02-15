70d5e320cbad13617f85fb923b712bfa9f03d064
BT_1E10_16 <sample@bths.edu>
2022-02-15 13:55:47 -0500

    yo

---
+++ src/IntenseW.java
@@ -0,0 +1,79 @@
+public class IntenseW {
+
+    public static final String BLACK = "\033[0;30m";   // BLACK
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String YELLOW = "\033[0;33m";  // YELLOW
+    public static final String BLUE = "\033[0;34m";    // BLUE
+    public static final String PURPLE = "\033[0;35m";  // PURPLE
+    public static final String CYAN = "\033[0;36m";    // CYAN
+    public static final String WHITE = "\033[0;37m";   // WHITE
+    public final String END_HOLE = 	"\u26F3";
+
+    public String name;
+
+
+    public IntenseW(String name) { this.name = name;}
+
+    public void hello() {
+        System.out.println(GREEN + "Hello this is" + CYAN + "JOE BIDEN" + RED + "And I am susssy " );
+    }
+
+    public void foodsMeLikey() {
+        System.out.println("Curry and Rice: " + "\uD83C\uDF5B");
+        System.out.println("Mangos: " + "\uD83E\uDD6D" );
+        System.out.println("EggPlant: " + "\uD83C\uDF46");
+        System.out.println("Peaches: " + "\uD83C\uDF51");
+    }
+
+    public void Animation() throws InterruptedException {
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "                              (**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "                         (**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "                    (**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "               (**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "          (**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "     (**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(BLUE);
+        System.out.println(END_HOLE + "(**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+
+        System.out.println(GREEN);
+        System.out.println("(**)___)___)___)^");
+        System.out.println(WHITE);
+        System.out.println("____________________________________________________________");
+        Thread.sleep(1000);
+    }
+}
